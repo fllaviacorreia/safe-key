1b396caeed5002d422a349471e933eedb511cb94
Flávia Correia <fllaviacorreia@outlook.com>
2024-10-29 10:04:38 -0300

    create input styles

---
+++ src/components/inputs/input/styles.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../../../theme";
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        paddingHorizontal: 20,
+        width:'100%',
+        height:60,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        marginVertical: 10,
+        borderWidth: 1,
+        paddingHorizontal: 15,
+        borderRadius: 12,
+        backgroundColor: theme.light.backgroundInputs,
+        borderColor: theme.light.placeholder,
+    },
+});
+
+export default styles
