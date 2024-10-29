faccc6c4670a18a450cb51ab27517a644cdc3f0f
Flávia Correia <fllaviacorreia@outlook.com>
2024-10-29 12:19:55 -0300

    include colors

--- theme.ts
+++ theme.ts
@@ -2,9 +2,11 @@ export const theme = {
     light: {
         background: "#F6F7F8",
         backgroundInputs: "#fff",
-        placeholder: "#B0B0B0"
+        placeholder: "#B0B0B0",
+        text: "#14281D",
+        success: "#73A580",
+        error: "#CD5334",
+        warning: "#F7C548",
     },
-    dark: {
-        text: "#",
-    },
+    dark: { },
 }
