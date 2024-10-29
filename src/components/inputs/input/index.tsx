224343e5de4c1f0a7f899a098cecdfc0c3b4b470
Flávia Correia <fllaviacorreia@outlook.com>
2024-10-29 12:13:25 -0300

    alter component input

--- src/components/inputs/input/index.tsx
+++ src/components/inputs/input/index.tsx
@@ -1,5 +1,6 @@
 import { Text, TextInput, TextInputProps, View } from "react-native";
 import styles from "./styles";
+import global from '../../../styles/global';
 
 type InputProps = TextInputProps & {
     title: string
@@ -7,8 +8,8 @@ type InputProps = TextInputProps & {
 export default function Input({ title, ...rest }: InputProps) {
     return (
         <View style={styles.container}>
-            <Text>{title}</Text>
-            <TextInput style={styles.input} {...rest}/>
+            <Text style={global.text}>{title}</Text>
+            <TextInput style={[styles.input, global.text]} {...rest}/>
         </View>
     )
 }
